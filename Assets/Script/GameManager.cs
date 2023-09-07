3191092695a42d6ba33d5ffb0000a1d534e3ed61
HanSangjun-study <139726046+Sangjunwow@users.noreply.github.com>
2023-09-07 18:32:15 +0900

    UI 다시

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,92 @@
+using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] enemyObjs;
+    public Transform[] spawnPoints;
+
+    public float maxSpawnDelay;
+    public float curSpawnDelay;
+
+    public GameObject player;
+    public TextMeshPro scoreText;
+    public Image[] lifeImage;
+    public GameObject gameOverSet;
+
+
+    void Update()
+    {
+        curSpawnDelay += Time.deltaTime;
+        if (curSpawnDelay > maxSpawnDelay)
+        {
+            spawnEnemy();
+            maxSpawnDelay = Random.Range(0.5f, 3f);
+            curSpawnDelay = 0;
+        }
+        Player playerLogic = player.GetComponent<Player>();
+        scoreText.text = string.Format("{0:n0}", playerLogic.score); // 999,999,999 포멧을 해주는 코드
+    }
+
+    void spawnEnemy()
+    {
+        int ranEnemy = Random.Range(0, 3);
+        int ranPoint = Random.Range(0, 9);
+       GameObject enemy = Instantiate(enemyObjs[ranEnemy],
+            spawnPoints[ranPoint].position,
+            spawnPoints[ranPoint].rotation
+            );
+        Rigidbody2D rigid = enemy.GetComponent<Rigidbody2D>();
+        Enemy enemyLogic = enemy.GetComponent<Enemy>();
+        
+        enemyLogic.player = player;
+
+        if (ranPoint == 5 || ranPoint == 6)
+        {
+            enemy.transform.Rotate(Vector3.back * 90);
+            rigid.velocity = new Vector2(enemyLogic.speed * (-1), -1);
+        }
+        else if (ranPoint == 7 || ranPoint == 8)
+        {
+            enemy.transform.Rotate(Vector3.forward * 90);
+            rigid.velocity = new Vector2(enemyLogic.speed, -1);
+        }
+        else
+        {
+            rigid.velocity = new Vector2(0,enemyLogic.speed*(-1));
+        }
+    }
+    public void RespawnPlayer()
+    {
+        Invoke("RespawnPlayerExe", 2f);
+    }
+    public void RespawnPlayerExe()
+    {
+        player.transform.position = Vector3.down * 3.5f;
+        player.SetActive(true);
+    }
+
+    public void updateLifeIcon(int life)
+    {
+        for(int index=0; index<3; index++)
+        {
+            lifeImage[index].color = new Color(1, 1, 1, 0); //이미지를 알파값을 조정함. 일단 전부 다 끔
+        }
+        for (int index = 0; index < life; index++)
+        {
+            lifeImage[index].color = new Color(1, 1, 1, 1); //이미지를 알파값을 조정함
+        }
+
+    }
+
+    public void GameOver()
+    {
+        gameOverSet.SetActive(true);
+    }
+}
